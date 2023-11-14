5e7202f06623f1cc84dec64013a2a1beaef9beda
dbroque1304 <dbroque1304@g.educaand.es>
2023-11-14 09:07:09 +0100

    Creación de la función pentagono y círculo

--- src/main/java/org/iesvdm/Main.java
+++ src/main/java/org/iesvdm/Main.java
@@ -1,7 +1,16 @@
 package org.iesvdm;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+    }
+    public static BigDecimal circulo(BigDecimal radio){
+        return BigDecimal.valueOf(Math.PI).multiply(radio.pow(2));
+    }
+    
+    public static BigDecimal pentagono(BigDecimal apotema, BigDecimal lado){
+        return ((lado.multiply(BigDecimal.valueOf(5))).multiply(apotema)).divide(BigDecimal.TWO);
     }
 }
