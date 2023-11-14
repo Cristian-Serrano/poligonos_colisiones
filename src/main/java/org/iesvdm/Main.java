3757b9d7d05db0a94071c6d6a923905c781f83eb
MarioRM12 <xtyponx@gmail.com>
2023-11-14 09:04:42 +0100

    ahora el pusheado de mairo

--- src/main/java/org/iesvdm/Main.java
+++ src/main/java/org/iesvdm/Main.java
@@ -1,7 +1,31 @@
 package org.iesvdm;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Miguel!");
+
+        System.out.println("hola david");
+
+        System.out.println("El area de un cuadrado de lado 45 es: " + areaCuadrado(45.0));
+        System.out.println("El area de un rectangulo de base 57 y altura 85 es: " + areaRectángulo(57d,85d));
     }
+
+    public static Double areaCuadrado(Double lado){
+        Double Area;
+
+        Area = lado * lado;
+
+        return Area;
+    }
+
+    public static Double areaRectángulo(Double Altura, Double Base) {
+        Double Area;
+
+        Area = Altura * Base;
+
+        return Area;
+    }
+
 }
